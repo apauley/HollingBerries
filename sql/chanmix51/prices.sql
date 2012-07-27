f22cce912e3521959c5bf0366711257ff4dd9ab3
Grégoire HUBERT <hubert.greg@gmail.com>
2012-07-27 16:59:07 +0200

    absolute schema path names.

--- sql/chanmix51/prices.sql
+++ sql/chanmix51/prices.sql
@@ -1,35 +1,31 @@
 -- price calculus SQL script
-
+BEGIN;
 CREATE SCHEMA import;
 SET search_path TO import, public;
 
 CREATE DOMAIN product_type AS integer CHECK ( VALUE BETWEEN 1000 AND 1999 );
 
-CREATE OR REPLACE FUNCTION is_fruit(product_type) RETURNS boolean AS $_$
-  SELECT $1 BETWEEN 1000 AND 1999;
-$_$ LANGUAGE sql IMMUTABLE;
-
-CREATE OR REPLACE FUNCTION is_apple(product_type) RETURNS boolean AS $_$
+CREATE OR REPLACE FUNCTION import.is_apple(product_type) RETURNS boolean AS $_$
   SELECT $1 BETWEEN 1100 AND 1199;
 $_$ LANGUAGE sql IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION is_banana(product_type) RETURNS boolean AS $_$
+CREATE OR REPLACE FUNCTION import.is_banana(product_type) RETURNS boolean AS $_$
   SELECT $1 BETWEEN 1200 AND 1299;
 $_$ LANGUAGE sql IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION is_berry(product_type) RETURNS boolean AS $_$
+CREATE OR REPLACE FUNCTION import.is_berry(product_type) RETURNS boolean AS $_$
   SELECT $1 BETWEEN 1300 AND 1399;
 $_$ LANGUAGE sql IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION is_trouble_supplier(integer) RETURNS boolean AS $_$
+CREATE OR REPLACE FUNCTION import.is_trouble_supplier(integer) RETURNS boolean AS $_$
   SELECT $1 IN (32, 101);
 $_$ LANGUAGE sql IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION is_premium_supplier(integer) RETURNS boolean AS $_$
+CREATE OR REPLACE FUNCTION import.is_premium_supplier(integer) RETURNS boolean AS $_$
   SELECT $1 IN (204, 219);
 $_$ LANGUAGE sql IMMUTABLE;
 
-CREATE TEMP TABLE produce (
+CREATE TABLE import.produce (
       supplier_id integer NOT NULL, 
       product_code product_type NOT NULL, 
       description varchar, 
@@ -40,10 +36,10 @@ CREATE TEMP TABLE produce (
       )
     ;
 
-\copy produce FROM 'produce.csv' WITH CSV HEADER QUOTE '"';
+\copy import.produce FROM 'produce.csv' WITH CSV HEADER QUOTE '"';
 
 WITH
-produce_markup (supplier_id, markup, price, max_sell_date, description, item_no) AS (
+produce_fruit (supplier_id, markup, price, max_sell_date, description, item_no) AS (
     SELECT 
         supplier_id,
         CASE 
@@ -52,7 +48,7 @@ produce_markup (supplier_id, markup, price, max_sell_date, description, item_no)
             WHEN is_berry(product_code)  THEN 1.55
             ELSE 1.5
         END AS markup,
-        cost_price,
+        cost_price AS price,
         CASE
             WHEN is_apple(product_code)  THEN delivery_date + interval '2 weeks'
             WHEN is_banana(product_code) THEN delivery_date + interval '5 days'
@@ -61,9 +57,9 @@ produce_markup (supplier_id, markup, price, max_sell_date, description, item_no)
         description,
         generate_series(1, unit_count) AS item_no
     FROM
-        produce
+        import.produce
     ),
-priced_produce (price, max_sell_date, description) AS (
+produce_supplier (price, max_sell_date, description) AS (
     SELECT
         CASE
             WHEN is_trouble_supplier(supplier_id) THEN greatest(0, (price * markup * 0.01) - 2)
@@ -76,8 +72,9 @@ priced_produce (price, max_sell_date, description) AS (
         END AS max_sell_date,
         description
     FROM
-        produce_markup
+        produce_fruit
     )
-SELECT 'R'||substr(to_char(price, '99990.00'), 2)||to_char(max_sell_date, 'YYYY/MM/DD')||substr(description,1 ,31) FROM priced_produce;
+SELECT 'R'||substr(to_char(price, '99990.00'), 2)||to_char(max_sell_date, 'YYYY/MM/DD')||substr(description,1 ,31) FROM produce_supplier;
 
 DROP SCHEMA import CASCADE;
+COMMIT;
