172a3d893f160bbd5bc9dcfe9300da5914010a76
Toby Kurien <toby@tobykurien.com>
2016-04-06 10:43:03 +0200

    cleanup up the implementation

--- rust/tobykurien/src/main.rs
+++ rust/tobykurien/src/main.rs
@@ -6,7 +6,7 @@ use chrono::*;
 use std::fs::File;
 use std::io::prelude::*;
 
-// ******** Business rules data ********
+// ******** Business rules and data ********
 enum Produce { Apple, Banana, Berry, Other }
 
 const MARKUP: [(Produce, f32); 4] = [ 
@@ -36,18 +36,7 @@ fn produce_type(produce: &ProduceRecord) -> usize {
 	}
 }
 
-// Delivery times for various produce
-fn delivery_days(produce: &ProduceRecord) -> i64 {
-	match produce.product_code {
-		1100...1199 => 14,
-		1200...1299 => 5,
-		_ => 7
-	}
-}
-
-
 // ******** Application code ********
-
 #[derive(RustcDecodable)]
 struct ProduceRecord {
     supplier_id: u32,
@@ -58,9 +47,10 @@ struct ProduceRecord {
     units: u32,
 }
 
-fn getSellPrice(produce: &ProduceRecord) -> f32 {
+fn get_sell_price(produce: &ProduceRecord) -> f32 {
 	let price = produce.unit_price as f32;
 	let prod_type = produce_type(&produce);
+	
 	let ret: f32 = if TROUBLE_SUPPLIERS.iter().any(|i| produce.supplier_id == *i) {
 		(price * MARKUP[prod_type].1 - 200.0)/ 100.0
 	} else if PREMIUM_SUPPLIERS.iter().any(|i| produce.supplier_id == *i) {
@@ -72,41 +62,36 @@ fn getSellPrice(produce: &ProduceRecord) -> f32 {
 	ret.max(0.0)
 }
 
-fn getSellBy(produce: &ProduceRecord) -> String {
-	let mut adj = delivery_days(produce);
+fn get_sell_by(produce: &ProduceRecord) -> String {
+	let prod_type = produce_type(&produce);
+	let mut adj = SHELF_LIFE[prod_type].1 as i64;
 
 	if TROUBLE_SUPPLIERS.iter().any(|i| produce.supplier_id == *i) {
 		adj = adj - 3
 	};
 	
-	let day = produce.delivery_day.clone();
-	let date: String = " 00:00:00 +00:00".chars().enumerate().fold(day, |res, (i, ch)| {
-        res + &format!("{}", ch)
-    });
+	let date = &format!("{} 00:00:00 +00:00", produce.delivery_day);
 	let dt = DateTime::parse_from_str(&date, "%Y/%m/%d %H:%M:%S %z")
 				.expect("Invalid date");
 	(dt + Duration::days(adj)).format("%Y/%m/%d").to_string()
 }
 
-fn processProduce(produce: ProduceRecord) -> String {
-	let lines: Vec<String> = (0..produce.units).map(|a| {
+fn process_produce(produce: ProduceRecord) -> String {
+	(0..produce.units).map(|a| {
 		let mut desc: String = produce.product_desc.clone();
 		desc.truncate(31);
-		format!("R{:8.2}{}{}", getSellPrice(&produce), getSellBy(&produce), desc)
-	}).collect();
-	
-	lines.as_slice().join("\n")
+		format!("R{:8.2}{}{}\n", get_sell_price(&produce), get_sell_by(&produce), desc)
+	}).collect()
 }
 
 fn main() {
     let mut f: File = File::create("pricefile.txt").expect("Can't open output file!");
-    let mut rdr = csv::Reader::from_file("../../produce.csv").unwrap();
+    let mut rdr = csv::Reader::from_file("../../produce.csv").expect("Can't open input file!");
     for record in rdr.decode() {
         let record: ProduceRecord = record.unwrap();
-        let output = processProduce(record);
+        let output = process_produce(record);
         if output.len() > 0 { 
-        	f.write(output.as_bytes()); 
-        	f.write(b"\n");
+        	f.write(output.as_bytes()).expect("Error writing output"); 
         }
     }
 }
